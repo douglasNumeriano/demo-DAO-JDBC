7cb398180f157d5c6ebe5e339569bc8f0979f8ce
Numeriano <douglas_rnmeriano@hotmail.com>
2020-08-04 22:36:01 -0300

    Creating project and git repository

---
+++ src/db/DB.java
@@ -0,0 +1,74 @@
+package db;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class DB{
+	
+	private static Connection conn=null;
+
+	//Método para conexão do banco 
+	public static Connection getConnection() {
+		if(conn ==  null ) {
+			try {
+			Properties props = loadProperties();
+			String url = props.getProperty("dburl");
+			
+			//aqui está sendo feito a conexão com o banco de dados
+			conn = DriverManager.getConnection(url, props);
+			}
+			catch(SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+		return conn;
+	}
+	
+	public static void closeConnection() {
+		if(conn != null) {
+			try {
+				conn.close();
+			}
+			catch(SQLException e){
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+	
+	private static Properties loadProperties() {
+		//Carrega as informações do arquivo db.properties
+		try(FileInputStream fs = new FileInputStream("db.properties")){
+			Properties props = new Properties();
+			props.load(fs);
+			return props;
+		}catch(IOException e) {
+			throw new DbException(e.getMessage());
+		}
+	}
+	
+	public static void closeStatement(Statement st) {
+		if(st != null) {
+			try {
+				st.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+	
+	public static void closeResultSet(ResultSet rs) {
+		if(rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+}
---
+++ src/db/DbIntergrityException.java
@@ -0,0 +1,10 @@
+package db;
+
+public class DbIntergrityException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+
+	public DbIntergrityException(String msg) {
+		super(msg);
+	}
+}
